6eab07f837e16936653be61bdbc48ee0a58b4adb
Konstantin Makarychev <kmakarychev@provectus.com>
2025-09-18 11:38:38 +0200

    Add awos npm package and Github Action for publishing

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const AWOS_ASCII = `
+   ░███      ░██       ░██     ░██████       ░██████   
+  ░██░██     ░██       ░██    ░██   ░██     ░██   ░██  
+ ░██  ░██    ░██  ░██  ░██   ░██     ░██   ░██         
+░█████████   ░██ ░████ ░██   ░██     ░██    ░████████  
+░██    ░██   ░██░██ ░██░██   ░██     ░██           ░██ 
+░██    ░██   ░████   ░████    ░██   ░██     ░██   ░██  
+░██    ░██   ░███     ░███     ░██████       ░██████   
+`;
+const AWOS_SUBTITLE = "Agentic Workflow Operating System for Coding Assistance";
+
+console.log(AWOS_ASCII);
+console.log(AWOS_SUBTITLE);
